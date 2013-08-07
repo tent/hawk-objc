c0dc1b9011646b884f38caf28551c7df1728b209
Jesse Stuart <jesse@jessestuart.ca>
2013-08-07 18:52:43 -0400

    Fix lib build

---
+++ libHawk/libHawk.h
@@ -0,0 +1,10 @@
+//
+//  libHawk.h
+//  libHawk
+//
+//  Created by Jesse Stuart on 8/7/13.
+//  Copyright (c) 2013 Tent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Hawk.h"
