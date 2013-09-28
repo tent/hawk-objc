fd82b34d34f71b1f1f48528edf719ad79d4d9790
Jesse Stuart <jesse@jessestuart.ca>
2013-09-28 13:41:46 -0400

    Update license

--- Hawk/HawkError.h
+++ Hawk/HawkError.h
@@ -3,7 +3,8 @@
 //  Hawk
 //
 //  Created by Jesse Stuart on 8/8/13.
-//  Copyright (c) 2013 Tent. All rights reserved.
+//  Copyright (c) 2013 Tent.is, LLC. All rights reserved.
+//  Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 //
 
 #import <Foundation/Foundation.h>
